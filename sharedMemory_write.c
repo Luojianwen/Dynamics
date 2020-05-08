663c5d3797bcba25ad4225580ba6784e6768f966
Jianwen Luo <luojianwen1123@gmail.com>
2020-05-08 09:52:27 +0800

    Update sharedMemory_write.c

--- sharedMemory_write.c
+++ sharedMemory_write.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
     int shmid;
     int ret;
     key_t key;
-    char *shmadd;
     double *num;
  
     //创建key值
@@ -22,9 +21,11 @@ int main(int argc, char *argv[])
     {
         perror("ftok");
     }
- 
-    //创建共享内存
-    shmid = shmget(key, BUFSZ, IPC_CREAT|0666);    
+    
+    //system("ipcs -m"); //查看共享内存
+    
+    //打开共享内存
+    shmid = shmget(key, BUFSZ, IPC_CREAT|0666);
     if(shmid < 0)
     {
         perror("shmget");
@@ -32,36 +33,14 @@ int main(int argc, char *argv[])
     }
  
     //映射
-    //shmadd = shmat(shmid, NULL, 0);
     num = shmat(shmid, NULL, 0);
-    //if(shmadd < 0)
     if(*num < 0)
     {
         perror("shmat");
-        _exit(-1);
+        exit(-1);
     }
- 
-    //拷贝数据至共享内存区
-    printf("copy data to shared-memory\n");
-    //bzero(shmadd, BUFSZ); // 共享内存清空
-    //strcpy(shmadd, "how are you, lh");
-
-    double cnt = 0.1;
-    num[0] = 10.132;
-    num[1] = 31.34;
-    num[2] = 3145.34;
-    num[3] = 31.6634;
-    while(1)
-    {
-        //bzero(num, BUFSZ); // 共享内存清空
-        num[0] = num[0] + cnt;
-        num[1] = num[1] + cnt;
-        num[2] = num[2] + cnt;
-        num[3] = num[3] + cnt;
-    }
-    /*
+  
     //分离共享内存和当前进程
-    //ret = shmdt(shmadd);
     ret = shmdt(num);
     if(ret < 0)
     {
@@ -75,7 +54,8 @@ int main(int argc, char *argv[])
  
     //删除共享内存
     shmctl(shmid, IPC_RMID, NULL);
-    */
+ 
+    //system("ipcs -m"); //查看共享内存
+    
     return 0;
 }
-
